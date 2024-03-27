e02fd08560a7723806ef38ae83d11885b73ee79c
Yaroslav <yar1k_k12@icloud.com>
2024-03-27 19:04:09 +0200

    feat: [CON-6] Reset password via e-mail

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 from users import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.UserListApiView.as_view(), name='users_list',),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('logout/', views.UserLogoutApiView.as_view(), name='logout'),
     path('delete/<int:pk>/', views.UserDeleteApiView.as_view(), name='delete'),
     path('confirm-email/<str:token>/<str:token2>/', views.UserConfirmEmailApiView.as_view(), name='confirm_email'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
